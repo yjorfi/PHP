04d1736c7a7ed5deb5901b395e243518e04bf682
yjorfi <9024527@student.zadkine.nl>
2025-01-09 10:37:52 +0100

    week 4 af

--- Week_4/dagdeel.php
+++ Week_4/dagdeel.php
@@ -1,11 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    echo"hou je bek";
-</body>
-</html>
+<?php
+$tijd = date("H");
+if ($tijd >= 06 && $tijd <12){
+    echo "Het is ochtend";
+}
+elseif ($tijd >= 12 && $tijd <18){
+    echo "Het is ochtend";
+}
+elseif($tijd >= 18 && $tijd <24){
+    echo "Het is ochtend";
+}
+else {echo "Het is nacht";}
+
+
+$dagtijd = match (true) {
+    $tijd >= 6 && $tijd < 12 => "ochtend",
+    $tijd >= 12 && $tijd < 18 => "middag",
+    $tijd >= 18 && $tijd < 24 => "avond",
+    $tijd >= 0 && $tijd < 6 => "avond",};
+echo "<br>";
+    echo "Het is nu $dagtijd";
+?>
