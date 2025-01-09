2c14a98902310a4b42e68c56e455707da4a5b902
yjorfi <9024527@student.zadkine.nl>
2025-01-09 09:43:01 +0100

    dobbelsteen opdracht

---
+++ Week_4/dobbelsteen.php
@@ -0,0 +1,8 @@
+<?php 
+$dobbelsteen = rand(1, 6);
+
+if ($dobbelsteen >= 4) {
+    echo "Gefeliciteerd! Je hebt gewonnen!";
+} else {
+    echo "Helaas, je hebt verloren. Probeer het nog eens!";
+}
---
+++ Week_4/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Dobbelsteen spel</h1>
+<?php include "dobbelsteen.php";?>
+
+</body>
+</html>
