a62a8f8b523c940e30e030cb3cebb33bfe9f2b4d
yjorfi <9024527@student.zadkine.nl>
2025-01-31 14:13:46 +0100

    opdracht 10

--- Week_7/index.php
+++ Week_7/index.php
@@ -6,3 +6,11 @@ if (!isset($_SESSION['visit_count'])) {
 $_SESSION['visit_count']++;
 
 echo "Deze pagina heb je al " . $_SESSION['visit_count'] . " keer bekeken voordat je de internet browser hebt afgesloten.";
+
+if (isset($_COOKIE['total_visits'])) {
+    $total_visits = $_COOKIE['total_visits'] + 1;
+} else {
+    $total_visits = 1;
+}
+setcookie('total_visits', $total_visits, time() + (365 * 24 * 60 * 60)); // 1 jaar geldig
+echo "<br> In totaal heb je deze pagina al " . $total_visits . " keer bekeken.";
